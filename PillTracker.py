7bc2854a81e9f0c24b2916cf6a0fa2ad90c1fc21
Akira <Nichelleevans7@gmail.com>
2026-08-04 20:21:13 -0400

    Update PillTracker.py

--- PillTracker.py
+++ PillTracker.py
@@ -1,3 +1,8 @@
+# Author: Akira Evans
+# Project: Pill Management System
+# Created: July 21, 2026
+# Description: Handles medication tracking and pill management.
+
 from PillManagement import PillManagement
 from TimeManagement import TimeManagement
 
